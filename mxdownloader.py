26b5ea31493b263854c12388d8dd547d5ea2a425
swappyison <88504971+swappyison@users.noreply.github.com>
2023-09-08 10:29:36 +0530

    Add files via upload

--- mxdownloader.py
+++ mxdownloader.py
@@ -1,36 +1,30 @@
 import os
-import yt_dlp
+import subprocess
+import re
 
-# Function to download videos from a list of links using yt-dlp
-def download_videos(links, output_dir):
-    ydl_opts = {
-        'outtmpl': os.path.join(output_dir, '%(title)s.%(ext)s'),
-    }
+# Read episode information from episode_links.txt
+with open('episode_links.txt', 'r') as file:
+    episode_info = file.read().splitlines()
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        for link in links:
-            ydl.download([link])
+# Regular expression pattern to match the required information
+pattern = r"Show: (.*), Season (\d+), Episode (.*), M3U8 Link: (.*)"
 
-# Specify the path to the episode_links.txt file
-links_file = 'episode_links.txt'
-
-# Specify the output directory where videos will be downloaded
-output_directory = 'downloaded_videos'
-
-# Create the output directory if it doesn't exist
-os.makedirs(output_directory, exist_ok=True)
-
-try:
-    with open(links_file, 'r') as file:
-        episode_links = file.read().splitlines()
-
-    if episode_links:
-        print(f"Downloading {len(episode_links)} videos...")
-        download_videos(episode_links, output_directory)
-        print("Download completed.")
+# Loop through each line in episode_info
+for line in episode_info:
+    match = re.match(pattern, line)
+    if match:
+        show, season, episode, m3u8_link = match.groups()
+        
+        # Define the output file name
+        output_file = f"{show.replace(' ', '_')}.S{season}.{episode}.{os.path.basename(m3u8_link).replace(' ', '_').replace('.m3u8', '.mp4')}"
+        
+        # Use yt-dlp to download the M3U8 link and convert it to mp4
+        subprocess.run([
+            'yt-dlp',
+            '-o', output_file,
+            m3u8_link
+        ])
     else:
-        print("No episode links found in the file.")
-except FileNotFoundError:
-    print(f"The file {links_file} does not exist.")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+        print(f"Invalid format: {line}")
+
+print("Downloads completed successfully.")
